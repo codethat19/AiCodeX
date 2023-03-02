4f3671b1defef260f95e0341cbbf2f36091e3303
codethat19 <aayushrastogi1997@gmail.com>
2023-03-02 12:44:45 +0530

    change api address to hosted

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //Fetch data from server => Bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://aicodex-server.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
